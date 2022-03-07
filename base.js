651fee1ccba7c86d9d29122e64a6f129b0f806dd
CarolinsRainbow <60187454+Carolinsrainbow@users.noreply.github.com>
2022-03-07 19:44:24 -0300

    ing

---
+++ base.js
@@ -0,0 +1,13 @@
+let primerNumero = prompt("Elige un número");
+
+let segundoNumero = prompt("Elige un segundo número");
+
+let suma = parseInt(primerNumero) + parseInt(segundoNumero);
+let resta = primerNumero - segundoNumero;
+let multiplicacion = primerNumero * segundoNumero;
+let division = primerNumero / segundoNumero;
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division);
